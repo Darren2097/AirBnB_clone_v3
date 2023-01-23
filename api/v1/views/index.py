02a13d8873dcc1da2690bff00e9d997aa589e0e4
Darren <darren2097@gmail.com>
2023-01-23 17:48:50 +0200

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Routing Functions"""
+
+from api.v1.views import app.views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def show_status():
+    """Shows the OK status"""
+
+    return jsonify({'status': 'OK'})
