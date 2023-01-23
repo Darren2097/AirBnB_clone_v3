df4fd1ad17d6e32e4d45a5067a0079f877a8efea
Darren <darren2097@gmail.com>
2023-01-23 18:15:11 +0200

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,19 @@ def show_status():
     """Shows the OK status"""
 
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Gets stats for models"""
+
+    stats_dict = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+
+    return jsonify(stats_dict)
