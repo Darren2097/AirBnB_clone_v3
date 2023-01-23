bdbdf9bae4c99cdf0df12319ef0da647455e15c4
Darren <darren2097@gmail.com>
2023-01-23 20:24:46 +0200

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from os import getenv
 from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
