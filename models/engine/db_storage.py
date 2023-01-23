090a0ff63085131870f39a474c35c13b64206f60
Darren <darren2097@gmail.com>
2023-01-23 20:36:12 +0200

    task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,13 +3,18 @@
 Contains the class DBStorage
 """
 
-from os import getenv
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, scoped_session
 import models
-from models.state import State
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 from models.city import City
-from models.base_model import Base
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.orm.exc import MultipleResultsFound
 
 
