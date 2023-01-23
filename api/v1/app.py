106ee21a4e39ce8b14531c5d200af35b3d395074
Darren <darren2097@gmail.com>
2023-01-23 17:40:56 +0200

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start api"""
+
+from os import getenv
+from flask import Flask, Blueprint, jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(error):
+    """Remove the current SQLAlchemy Session"""
+
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST', default='0.0.0.0'),
+            port=getenv('HBNB_API_PORT', default=5000), threaded=True)
